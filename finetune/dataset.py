fix dataset z-score leakage

finetune/dataset.py
@@ -111,18 +111,24 @@ class QlibDataset(Dataset):
 
         # Extract the sliding window from the dataframe.
         df = self.data[symbol]
-        end_idx = start_idx + self.window
+        end_idx = start_idx + self.window # this is time window, T
         win_df = df.iloc[start_idx:end_idx]
 
         # Separate main features and time features.
         x = win_df[self.feature_list].values.astype(np.float32)
         x_stamp = win_df[self.time_feature_list].values.astype(np.float32)
 
-        # Perform instance-level normalization.
-        # Be careful about data leakage here, because x contains time span in this window.
-        # We use this window to predict the future, not to predict within this window!
-        x_mean, x_std = np.mean(x, axis=0), np.std(x, axis=0)
-        x = (x - x_mean) / (x_std + 1e-5)
+        # # Perform instance-level normalization.
+        # # Be careful about data leakage here, because x contains time span in this window.
+        # # We use this window to predict the future, not to predict within this window!
+        # x_mean, x_std = np.mean(x, axis=0), np.std(x, axis=0)
+        # x = (x - x_mean) / (x_std + 1e-5)
+        # x = np.clip(x, -self.config.clip, self.config.clip)
+
+        L = self.config.lookback_window  # only use the lookback window for normalization
+        x_hist = x[:L]
+        x_hist_mean, x_hist_std = np.mean(x_hist, axis=0), np.std(x_hist, axis=0)
+        x = (x - x_hist_mean) / (x_hist_std + 1e-5)
         x = np.clip(x, -self.config.clip, self.config.clip)
 
         # Convert to PyTorch tensors.

model/kronos.py
@@ -56,12 +56,12 @@ class KronosTokenizer(nn.Module, PyTorchModelHubMixin):
         self.embed = nn.Linear(self.d_in, self.d_model)
         self.head = nn.Linear(self.d_model, self.d_in)
 
-        # Encoder Transformer Blocks
+        # Encoder Transformer Blocks, causal
         self.encoder = nn.ModuleList([
             TransformerBlock(self.d_model, self.n_heads, self.ff_dim, self.ffn_dropout_p, self.attn_dropout_p, self.resid_dropout_p)
             for _ in range(self.enc_layers - 1)
         ])
-        # Decoder Transformer Blocks
+        # Decoder Transformer Blocks, causal
         self.decoder = nn.ModuleList([
             TransformerBlock(self.d_model, self.n_heads, self.ff_dim, self.ffn_dropout_p, self.attn_dropout_p, self.resid_dropout_p)
             for _ in range(self.dec_layers - 1)
@@ -261,7 +261,7 @@ class Kronos(nn.Module, PyTorchModelHubMixin):
         x = self.token_drop(x)
 
         for layer in self.transformer:
-            x = layer(x, key_padding_mask=padding_mask)
+            x = layer(x, key_padding_mask=padding_mask) # padding_mask is for hiding the padding place, which has no meaning in attention calculation. not causal mask!!!
 
         x = self.norm(x)
 