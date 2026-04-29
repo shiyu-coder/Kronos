0f46ca0e26095203b328d8171743d1494241691d
sutarrohit <rohitsutar082@gmail.com>
2026-04-29 08:10:59 +0530

    fix: allow CORS for all origins

--- web/server/main.py
+++ web/server/main.py
@@ -33,7 +33,7 @@ app.include_router(results.router, prefix="/api", tags=["Results"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
